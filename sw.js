e8c475a000ba7c74ca79bb69c0039024c1961000
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-06-28 14:50:00 +0200

    Est. caché: cache with network fallback

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
+/*
+Optimizar el caché es muy importante. Para hacerlo organizamos el caché en tres elementos
+    - Estática: app shell
+    - Dinámica:
+    - Inmutable: p.e. la librería de bootstrap, ya que una versión nunca cambia
+*/
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
 // El app shell son aquellos archivos que nuestra página web necesita si o si para
 // funcionar. Como el propio nombre indica, es el cascarón de nuestro programa
 // El app shell se añade a la memoria en el momento de la instalación
 self.addEventListener('install', e => {
-    const cacheShell = caches.open('cache-shell')
+    const cacheShell = caches.open(CACHE_STATIC_NAME)
         .then( cache => {
             return cache.addAll([
                 // es importante de añadir esta primera ruta, ya que el usuario puede
@@ -52,6 +62,45 @@ self.addEventListener('fetch', e => {
     En segundo lugar tenemos la estrategia del cache with network fallback
 
     Es una estrategia que primero comprueba si tenemos el archivo guardado en el caché,
-    pero si este no lo está, lo busca en
+    pero si este no lo está, lo busca en internet
+
+    El inconveniente con esta estrategia es que se mezcla el app shell con otras peticiones,
+    por lo que el caché puede crecer muhco y hacer que nuestro usuario borre la aplicación
+
+    Este inconveniente se soluciona optimizando el caché
     */
+    const estCache2 = caches.match(e.request)
+        .then(res => {
+            // Como no podemos manejar los 404 con el .catch() debemos comprobar si la 
+            // petición se pudo llevar a cabo
+            // en caso de que exista, simplemente la devolvemos
+            if (res) return res;
+
+            console.log('No existe ', e.request.url);
+            // en caso de que no existe el archivo que se está pidiendo
+            // debemos hacer una petición para que nos la traiga
+            return fetch(e.request)
+                // en caso de que sí pueda encontrar la petición, nos la devuelve
+                // y la grabamos en el caché para que no se tenga que volver a 
+                // descargar
+                // Para la optimización del caché, como estos archivos supuestamente
+                // no forman parte del app shell, los guardaremos en la memoria dinámica
+                .then(res => {
+                    caches.open(CACHE_DYNAMIC_NAME)
+                        .then(cache => {
+                            // añadimos la petición como elemento del caché
+                            
+                            cache.put(e.request, res);
+                        });
+                    // debido a algún fallo en la nueva versión de las promesas, como el res
+                    // lo repetimos dos veces, algo falla. Pero clonando la respuesta lo
+                    // resolvemos
+                    return res.clone();
+                })
+                .catch(console.log);
+
+
+        });
+
+    e.respondWith(estCache2);
 })
